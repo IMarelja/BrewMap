4b88647a1e7343beb98a8fb1850889308d7c3117
ZiziCap <zara.capps@yahoo.co.uk>
2026-04-01 12:11:19 +0200

    AuthController inital version, needs database context to work

--- WebAPI/BrewMapAPI/BrewMapAPI/DTO/Auth/RegisterRequest.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/DTO/Auth/RegisterRequest.cs
@@ -1,6 +1,17 @@
-﻿namespace BrewMapAPI.DTO.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrewMapAPI.DTO.Auth
 {
     public class RegisterRequest
     {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
     }
 }
