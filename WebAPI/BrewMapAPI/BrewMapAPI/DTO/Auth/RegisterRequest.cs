5a7e3772df24db1d80d81a31babc713b995d2394
IMarelja <imarelja@algebra.hr>
2026-04-17 01:26:58 +0200

    Enforcement to be an email address

--- WebAPI/BrewMapAPI/BrewMapAPI/DTO/Auth/RegisterRequest.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/DTO/Auth/RegisterRequest.cs
@@ -5,6 +5,7 @@ namespace BrewMapAPI.DTO.Auth
     public class RegisterRequest
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         
         [Required]
