eaec7d6023573d31ef48d694c1ade67111bec0df
IMarelja <imarelja@algebra.hr>
2026-04-20 18:42:12 +0200

    User repository

--- WebAPI/BrewMapAPI/BrewMapAPI/Repository/User/IUserRepo.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/Repository/User/IUserRepo.cs
@@ -1,10 +1,11 @@
-using BrewMapAPI.DTO.User;
 using BrewMapAPI.Models;
 
 namespace BrewMapAPI.Repository.Users;
 
 public interface IUserRepo
 {
-    User? GetUserById(string id);
-    
+    Task<User?> GetUserById(string id);
+    Task<bool> UpdatePassword(string id, string newPasswordHash, string newPasswordSalt);
+    Task<bool> UpdateEmail(string id, string newEmail);
+    Task<bool> DeleteUser(string id);
 }
