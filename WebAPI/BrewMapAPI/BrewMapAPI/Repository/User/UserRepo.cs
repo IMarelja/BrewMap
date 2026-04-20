e43c936979d8801e6100c7dc7248868e22da5b76
IMarelja <imarelja@algebra.hr>
2026-04-20 16:26:01 +0200

    template controller, repo and service

---
+++ WebAPI/BrewMapAPI/BrewMapAPI/Repository/User/UserRepo.cs
@@ -0,0 +1,11 @@
+using BrewMapAPI.Models;
+
+namespace BrewMapAPI.Repository.Users;
+
+public class UserRepo : IUserRepo
+{
+    public User? GetUserById(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
