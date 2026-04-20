1a8364a2a086585070c354c9963332b4b35fe989
ValentinaRadošAlgebra <vrados@algebra.hr>
2026-04-20 20:59:12 +0200

    fixed the update

--- WebAPI/BrewMapAPI/BrewMapAPI/DTO/Location/UpdateLocation.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/DTO/Location/UpdateLocation.cs
@@ -6,36 +6,27 @@ namespace BrewMapAPI.DTO.Location
 {
     public class UpdateLocation : IValidatableObject
     {
-
-        // MUST STAY LOWERCASE
         private static readonly string[] RequiredDays =
         {
             "monday", "tuesday", "wednesday",
             "thursday", "friday", "saturday", "sunday"
         };
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string? Description { get; set; }
-        public Address Address { get; set; }
+        public Address? Address { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
-        public string CategoryTag { get; set; }
-        public List<string> PaymentOptionTags { get; set; }
+        public string? CategoryTag { get; set; }
+        public List<string>? PaymentOptionTags { get; set; }
         public Contact? Contact { get; set; }
         public string? EditComment { get; set; }
-
-        [Required]
-        public Dictionary<string, DayOpeningHours> OpeningHours { get; set; }
+        public Dictionary<string, DayOpeningHours>? OpeningHours { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (OpeningHours == null)
-            {
-                yield return new ValidationResult(
-                    "OpeningHours is required.",
-                    new[] { nameof(OpeningHours) });
                 yield break;
-            }
 
             foreach (var day in RequiredDays)
             {
