14b7f05edbfe7d1aa65869d38706d52ade478706
IMarelja <imarelja@algebra.hr>
2026-04-24 17:57:43 +0200

    no more DTO and model errors, thank god

--- WebAPI/BrewMapAPI/BrewMapAPI/DTO/Moderation/ModerationDTO.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/DTO/Moderation/ModerationDTO.cs
@@ -3,16 +3,16 @@ namespace BrewMapAPI.DTO.Moderation
     public class ModerationResult
     {
         public bool Success { get; set; }
-        public string Message { get; set; }
+        public string Message { get; set; } = String.Empty;
         public DateTime ActionPerformedAt { get; set; } = DateTime.UtcNow;
     }
 
     public class ModeratedUserInfo
     {
-        public string Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Role { get; set; }
+        public string Id { get; set; } = String.Empty;
+        public string Username { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Role { get; set; } = String.Empty;
         public DateTime CreatedAt { get; set; }
     }
 }
