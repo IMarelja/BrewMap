22a5f6f849b3b2955b91e4da6d550115b09533e0
IMarelja <imarelja@algebra.hr>
2026-04-26 23:06:23 +0200

    Creating a review for location and drink test

---
+++ WebAPI/BrewMapAPI/BrewMapEndpointUnitTest/ViewModels/Location/ReadLocationViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrewMapEndpointUnitTest.ViewModels.Location;
+
+public class ReadLocationViewModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public double AverageRating { get; set; }
+    public int TotalReviews { get; set; }
+}
