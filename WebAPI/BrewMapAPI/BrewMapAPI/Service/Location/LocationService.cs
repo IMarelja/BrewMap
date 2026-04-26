d944c5d7d726636fea7aa59e08b475cbb8c0b603
IMarelja <imarelja@algebra.hr>
2026-04-27 00:30:52 +0200

    Locations filter by list of categories

--- WebAPI/BrewMapAPI/BrewMapAPI/Service/Location/LocationService.cs
+++ WebAPI/BrewMapAPI/BrewMapAPI/Service/Location/LocationService.cs
@@ -115,7 +115,7 @@ namespace BrewMapAPI.Service.Location
         }
 
         public async Task<IEnumerable<ReadLocation>> SearchAsync(
-            string? query, double? minRating, string? drinkQuery, List<string>? paymentOptionTags, double? centerLatitude, double? centerLongitude, double radiusMeters)
+            string? query, double? minRating, string? drinkQuery, List<string>? categoryTags, List<string>? paymentOptionTags, double? centerLatitude, double? centerLongitude, double radiusMeters)
         {
             var normalizedQuery = query?.Trim();
             var normalizedDrinkQuery = drinkQuery?.Trim();
@@ -123,6 +123,7 @@ namespace BrewMapAPI.Service.Location
             var locations = (await _repo.SearchAsync(
                 null,
                 minRating,
+                categoryTags,
                 paymentOptionTags,
                 centerLatitude,
                 centerLongitude,
