99d321b19308bda1d1359c283556bde2f2215189
IMarelja <imarelja@algebra.hr>
2026-04-28 20:18:53 +0200

    Unit testing for the best drink

---
+++ WebAPI/BrewMapAPI/BrewMapEndpointUnitTest/ViewModels/Drinks/ReadBestDrinkViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrewMapEndpointUnitTest.ViewModels.Drinks;
+
+public class ReadBestDrinkViewModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public double Rating { get; set; }
+}
